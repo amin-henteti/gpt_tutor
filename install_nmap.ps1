365ba8413089456342634c3c7039f49e5dd0d1cc
amin henteti <aminhenteti21@gmail.com>
2023-05-26 19:04:13 +0200

    add ethercap install

--- install_nmap.ps1
+++ install_nmap.ps1
@@ -30,3 +30,36 @@ if (Get-Command "nmap" -ErrorAction SilentlyContinue) {
 } else {
     Write-Host "Failed to install Nmap."
 }
+
+
+# Define the download URL for Ettercap
+$ettercapDownloadUrl = "https://github.com/Ettercap/ettercap/releases/download/v0.8.3/ettercap-0.8.3-windows.zip"
+
+# Define the installation directory
+$installDirectory = Join-Path $env:ProgramFiles "Ettercap"
+
+# Create the installation directory if it doesn't exist
+Write-Host "Creating installation directory..."
+New-Item -ItemType Directory -Path $installDirectory -ErrorAction SilentlyContinue | Out-Null
+
+# Download the Ettercap zip file
+$zipFilePath = Join-Path $installDirectory "ettercap.zip"
+Write-Host "Downloading Ettercap..."
+Invoke-WebRequest -Uri $ettercapDownloadUrl -OutFile $zipFilePath
+
+# Extract the contents of the zip file
+Write-Host "Extracting Ettercap..."
+$ettercapFolder = Join-Path $installDirectory "Ettercap"
+Expand-Archive -Path $zipFilePath -DestinationPath $ettercapFolder -Force
+
+# Clean up the downloaded zip file
+Write-Host "Cleaning up..."
+Remove-Item -Path $zipFilePath -Force
+
+# Add Ettercap to the system's PATH environment variable
+$envPath = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
+$updatedEnvPath = $envPath + ";" + $ettercapFolder
+
+[System.Environment]::SetEnvironmentVariable("Path", $updatedEnvPath, "Machine")
+
+Write-Host "Ettercap installed successfully to $ettercapFolder"
