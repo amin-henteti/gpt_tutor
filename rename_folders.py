db9f7aaf31cf9f3b39a91cb028aa7f7a180aa653
amin henteti <aminhenteti21@gmail.com>
2023-05-27 04:33:27 +0200

    refactor rename_files_in_folder

--- rename_folders.py
+++ rename_folders.py
@@ -10,12 +10,11 @@ def split_name(name):
 
     Returns:
         int: The prefix of the name.
-        base: The remaining name
+        base: The remaining name.
     """
-    text = Path(name).parts[-1]
+    text = Path(name).name
     pattern = r'^(\d{1,3})\.? ([\w\s&\-,\.\(\)\[\]\^\+\'\!_#;\–]+)$'
     match = re.search(pattern, text)
-    # print("got match for", name)
     if match:
         prefix = match.group(1)
         base = match.group(2)
@@ -23,9 +22,35 @@ def split_name(name):
         index = re.search(r"[a-zA-Z]", base).start()
         base = base[index:]
     else:
-        raise AssertionError(f"pattern not respected for '{text}'")
-    # convert the prefix to an integer
+        raise AssertionError(f"Pattern not respected for '{text}'")
     return int(prefix), base
+
+
+def rename_files_in_folder(folder_path):
+    """Renames files in the given folder to have a zero-padded prefix.
+
+    Args:
+        folder_path (Path): The path of the folder.
+
+    Returns:
+        None.
+    """
+    folder = Path(folder_path)
+    # remove docx, pdf,txt,... files that have the pattern \d.\d *
+    files = [f for f in folder.iterdir() if f.is_file() and not f.endswith(("docx", "pdf", "jpg", "doc", "xlsx", "txt", "html"))]
+    max_prefix = max([split_name(f.name)[0] for f in files])
+    padding_length = len(str(max_prefix))
+    
+    for file in files:
+        try:
+            prefix, base = split_name(file.name)
+            new_prefix = str(prefix).zfill(padding_length)
+            file_new_name = f"{new_prefix}. {base}"
+            file_new = Path(file.parent, file_new_name)
+            file.rename(file_new)
+            print(f"Renamed file {file}\n ----> {file_new}\n{'-' * 80}")
+        except Exception as e:
+            print(f"Failed to rename file {file}: {e}")
 
 
 def rename_subfolders(base_path, folder_name):
@@ -40,12 +65,13 @@ def rename_subfolders(base_path, folder_name):
     """
     folder_path = Path(base_path, folder_name)
     subfolders = [d for d in folder_path.iterdir() if d.is_dir()]
+    
+    max_prefix = max([split_name(d.name)[0] for d in subfolders])
+    padding_length = len(str(max_prefix))
+    print("padding_length", padding_length)
     for subfolder in subfolders:
         # pprint([split_name(d.name) for d in subfolders][:5])
         # should be declared (and recalculated) here as we use the same names when called from within files rename
-        max_prefix = max([split_name(d.name)[0] for d in subfolders])
-        padding_length = len(str(max_prefix))
-        print("padding_length", padding_length)
         try:
             prefix, base = split_name(subfolder.name)
             new_prefix = str(prefix).zfill(padding_length)
@@ -56,32 +82,15 @@ def rename_subfolders(base_path, folder_name):
             subfolder_new = Path(subfolder.parent, subfolder_new_name)
             subfolder.rename(subfolder_new)
             print(f"{80*'='}\nRenamed folder {subfolder}\n ----> {subfolder_new}\n{80*'-'}")
-            files = [d for d in subfolder_new.iterdir() if d.is_file()]
-            # remove docx, pdf,txt,... files that have the pattern \d.\d *
-            files = [file for file in files if not str(file).endswith(("docx", "pdf", "jpg", "doc", "xlsx", "txt", "html"))]
-            for file in files:
-                max_prefix = max([split_name(f.name)[0] for f in files])
-                padding_length = len(str(max_prefix))
-                try:
-                    prefix, base = split_name(file.name)
-                    new_prefix = str(prefix).zfill(padding_length)
-                    file_new_name = f"{new_prefix}. {base}"
-                    file.rename(Path(file.parent, file_new_name))
-                    print(f"Renamed file {file}\n ----> {Path(file.parent, file_new_name)}\n{80*'-'}")
-                except Exception as e:
-                    print(f"Failed to rename file {file}: {e}")
+            rename_files_in_folder(subfolder_new)
         except Exception as e:
-            raise Exception(f"Failed to rename file {subfolder}: {e}")
+            print(f"Failed to rename folder {subfolder}: {e}")
 
 
 if __name__ == "__main__":
     base_path = Path(__file__).parent
-    folder_names = (
-        "Udemy - Practical Reinforcement Learning using Python - 8 AI Agents 2021-3",
-        "Udemy – Reinforcement Learning beginner to master – AI in Python 2022-10",
-        "Udemy - Logging in Kubernetes with EFK Stack 2022-3"
-    )
+    folder_names = ["learn"]
+    
     for folder_name in folder_names:
         rename_subfolders(base_path, folder_name)
-
         print(f"Finished renaming directory {folder_name} and its subdirectories.")
